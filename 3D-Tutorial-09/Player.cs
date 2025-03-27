0c6f93e0fe84c608bd5cbcc87ad8e343e69556a9
Gaëtan Blaise-Cazalet <gaetan.blaisecazalet@gmail.com>
2025-03-27 15:45:41 +0100

    AZERTY controls for tutorial 09

--- 3D-Tutorial-09/Player.cs
+++ 3D-Tutorial-09/Player.cs
@@ -46,9 +46,8 @@ namespace Tutorial_09
 
         private void HandlingInput(double dt)
         {
-            // Controls
             KeyboardState state = Keyboard.GetState();
-            if (state.IsKeyDown(Keys.W))
+            if (state.IsKeyDown(Keys.W) || state.IsKeyDown(Keys.Z))
             {
                 speedY += ACCELERATION_RATE * (float)dt;
             }
@@ -61,8 +60,7 @@ namespace Tutorial_09
                 speedY = MathF.Sign(speedY) * MAX_SPEED;
             }
 
-
-            if (state.IsKeyDown(Keys.A))
+            if (state.IsKeyDown(Keys.A) || state.IsKeyDown(Keys.Q))
             {
                 speedX -= ACCELERATION_RATE * (float)dt;
             }
