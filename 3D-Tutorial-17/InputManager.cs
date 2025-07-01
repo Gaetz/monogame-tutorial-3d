1583265b44f10d9216248e9dda48ace40f60f608
Gaetz <gaetz@rufflerim.com>
2025-07-01 07:58:44 +0200

    Step 17: advanced input manager

--- 3D-Tutorial-17/InputManager.cs
+++ 3D-Tutorial-17/InputManager.cs
@@ -1,10 +1,4 @@
 ﻿using Microsoft.Xna.Framework.Input;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Tutorial_17
 {
@@ -12,29 +6,102 @@ namespace Tutorial_17
     {
         KeyboardState previousState;
         KeyboardState currentState;
-        bool isPreviousStateNull = true;
+        GamePadState previousGamePadState;
+        GamePadState currentGamePadState;
+        MouseState previousMouseState;
+        MouseState currentMouseState;
 
-        public void Update(KeyboardState state)
+        bool isPreviousStateNull = true;
+        bool isPreviousGamePadStateNull = true;
+        bool isPreviousMouseStateNull = true;
+
+        public void Update(KeyboardState state, GamePadState gamePadState)
         {
             currentState = state;
+            currentGamePadState = gamePadState;
         }
 
         public void EndUpdate()
         {
             previousState = currentState;
+            previousGamePadState = currentGamePadState;
             isPreviousStateNull = false;
+            isPreviousMouseStateNull = false;
+            isPreviousGamePadStateNull = false;
         }
 
-        public bool IsKeyPressed(Keys key)
+        private bool IsKeyPressed(Keys key)
         {
             return currentState.IsKeyDown(key) 
                 && previousState.IsKeyUp(key) 
                 && !isPreviousStateNull;
         }
 
-        public bool IsKeyDown(Keys key)
+        private bool IsKeyDown(Keys key)
         {
             return currentState.IsKeyDown(key);
+        }
+
+        private bool IsButtonPressed(Buttons button)
+        {
+            return currentGamePadState.IsButtonDown(button) 
+                && previousGamePadState.IsButtonUp(button) 
+                && !isPreviousGamePadStateNull;
+        }
+
+        private bool IsButtonDown(Buttons button)
+        {
+            return currentGamePadState.IsButtonDown(button);
+        }
+
+        private bool IsMouseLeftButtonPressed()
+        {
+            return currentMouseState.LeftButton == ButtonState.Pressed 
+                && previousMouseState.LeftButton == ButtonState.Released 
+                && !isPreviousMouseStateNull;
+        }
+
+        private bool IsMouseLeftButtonDown()
+        {
+            return currentMouseState.LeftButton == ButtonState.Pressed;
+        }
+
+        public bool IsValidationActionPressed()
+        {
+            return IsKeyPressed(Keys.Enter) || IsKeyPressed(Keys.Space) 
+                || IsButtonPressed(Buttons.A) || IsButtonPressed(Buttons.RightShoulder) 
+                || IsButtonPressed(Buttons.RightTrigger);
+        }
+
+        public bool IsShootActionPressed()
+        {
+            return IsMouseLeftButtonPressed() || IsButtonPressed(Buttons.RightTrigger)
+                || IsButtonPressed(Buttons.RightShoulder) || IsButtonPressed(Buttons.A)
+                || IsKeyPressed(Keys.Space) || IsKeyPressed(Keys.Enter);
+        }
+
+        public bool IsUpAction() 
+        {
+            return IsKeyDown(Keys.W) || IsKeyDown(Keys.Up) || IsKeyDown(Keys.Z)
+                || IsButtonDown(Buttons.DPadUp) || IsButtonDown(Buttons.LeftThumbstickUp);
+        }
+
+        public bool IsDownAction() 
+        {
+            return IsKeyDown(Keys.S) || IsKeyDown(Keys.Down) 
+                || IsButtonDown(Buttons.DPadDown) || IsButtonDown(Buttons.LeftThumbstickDown);
+        }
+
+        public bool IsLeftAction() 
+        {
+            return IsKeyDown(Keys.A) || IsKeyDown(Keys.Left) || IsKeyDown(Keys.Q)
+                || IsButtonDown(Buttons.DPadLeft) || IsButtonDown(Buttons.LeftThumbstickLeft);
+        }
+
+        public bool IsRightAction() 
+        {
+            return IsKeyDown(Keys.D) || IsKeyDown(Keys.Right) 
+                || IsButtonDown(Buttons.DPadRight) || IsButtonDown(Buttons.LeftThumbstickRight);
         }
     }
 }
